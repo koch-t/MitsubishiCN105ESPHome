2c56f0fbf84ab081711f009b48ab7d5b4590923b
Thomas Koch <thakoch@gmail.com>
2026-09-03 23:24:48 -0400

    Fix CUSTOM_MILLIS include order

--- components/cn105/Globals.h
+++ components/cn105/Globals.h
@@ -5,8 +5,8 @@
 #include "Arduino.h"
 #endif
 
-#include <esphome.h>
-#include "esphome/components/uart/uart.h"
-
 #define CUSTOM_MILLIS esphome::millis()
 #define CUSTOM_DELAY(x) esphome::delay(x)
+
+#include <esphome.h>
+#include "esphome/components/uart/uart.h"
